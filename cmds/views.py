e490ac62d999c390412e9471a4239cdf980cc83a
yehaifeng <769358362@qq.com>
2017-11-29 18:12:24 +0800

    day1

--- cmds/views.py
+++ cmds/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def query(request):
+    if request.POST == 'POST':
+        pass
+    render(request, 'query.html', context=book_data)
