86060d02e3f74da6656d52925d025849121304f4
yehaifeng <769358362@qq.com>
2017-11-30 11:29:29 +0800

    day1-yehaifeng:add tables 'BOOK_INFO'

--- cmds/models.py
+++ cmds/models.py
@@ -2,5 +2,19 @@ from django.db import models
 
 # Create your models here.
 class BOOK_INFO(models.Model):
-    id = models.IntegerField(unique=True, auto_created=True)
-    name = models.CharField(max_length=)
+    book_id = models.AutoField(primary_key=True, db_column='book_id')
+    book_name = models.CharField(max_length=255, null=False, blank=False, db_index=True, db_column='book_name')
+    book_author = models.CharField(max_length=255, null=True, default=None, db_column='book_author')
+    book_translator = models.CharField(max_length=255, null=True, default=None, db_column='book_translator')
+    book_pages = models.IntegerField(null=True, default=None, db_column='book_pages')
+    book_publisher = models.CharField(max_length=255, null=True, default=None, db_column='book_publisher')
+    book_class = models.CharField(max_length=100, null=True, default=None, db_column='book_class')
+    book_publish_date = models.DateField(null=True, default=None, db_column='book_publish_date')
+    book_buy_date = models.DateField(auto_now=True, db_column='book_buy_date')
+    book_description = models.TextField(db_column='book_description')
+
+    def __str__(self):
+        return self.book_name
+
+    class Meta:
+        db_table = 'BOOK_INFO'
