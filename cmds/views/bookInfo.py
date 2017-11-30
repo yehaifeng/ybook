3a5cf8c1038c1ede20a8559ae671a7b98361edcf
yehaifeng <769358362@qq.com>
2017-11-30 17:43:55 +0800

    day1-yehaifeng:last

--- cmds/views/bookInfo.py
+++ cmds/views/bookInfo.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # _*_coding:utf-8_*_
 
+from django.views.generic import View
+from django.shortcuts import render, HttpResponse
+from cmds.pkgs.divpage import MarkPage,PageInfo
 
-from django.shortcuts import render
 
-
-
+class Query(View):
+    def __init__(self):
+        pass
+    def get(self):
+        pass
+    def post(self):
+        pass
